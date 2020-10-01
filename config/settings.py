e95551511a24d10060671d78526496e8eb11cb6f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-02 00:52:16 +0600

    create room list

--- config/settings.py
+++ config/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
-MEDIA_URL = "/media/"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = "users.User"
