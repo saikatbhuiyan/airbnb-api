3650d063a03c473015d191ba4a4e86e8bda8b214
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-11 21:55:26 +0600

    UserViewset complete

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "users"
 
-urlpatterns = [
-    path("", views.UsersView.as_view()),
-    path("token/", views.login),
-    path("me/", views.MeView.as_view()),
-    path("<int:pk>/", views.user_detail),
-    path("me/favs/", views.FavsView.as_view()),
-]
+router = DefaultRouter()
+router.register("", views.UsersViewSet)
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path("", views.UsersView.as_view()),
+#     path("token/", views.login),
+#     path("me/", views.MeView.as_view()),
+#     path("<int:pk>/", views.user_detail),
+#     path("me/favs/", views.FavsView.as_view()),
+# ]
--- users/views.py
+++ users/views.py
@@ -7,11 +7,71 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.decorators import action
 
 from .models import User
 from .serializers import UserSerializer
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
+from .permissions import IsSelf
+
+
+class UsersViewSet(ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif (
+            self.action == "create"
+            or self.action == "retrieve"
+            or self.action == "favs"
+        ):
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsSelf | IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=["post"])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt, "id": user.pk})
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    @action(detail=True)
+    def favs(self, request, pk):
+        user = self.get_object()
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    @favs.mapping.put
+    def toggle_favs(self, request, pk):
+        pk = request.data.get("pk", None)
+        user = self.get_object()
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class UsersView(APIView):
