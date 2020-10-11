e970867764e6b94bf8eda7d279900398b6b8f454
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-10-11 20:44:08 +0600

    create RoomViewSet using modelviewset and IsOwner permission

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
-# from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter
 
 from . import views
-# from . import viewsets
 
 app_name = 'rooms'
 
-urlpatterns = [
-    # path("list/", views.rooms_view),
-    # path("list/", views.ListRoomsView.as_view()),
-    # path("<int:pk>/", views.DetailRoomView.as_view()),
-    path("search/", views.room_search, name="search"),
-    path("", views.RoomsView.as_view()),
-    path("<int:pk>/", views.RoomView.as_view()),
-]
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
 
-# router = DefaultRouter()
-# router.register("", viewsets.RoomViewset, basename="room")
+urlpatterns = router.urls
 
-# urlpatterns = router.urls
+
+# urlpatterns = [
+#     # path("list/", views.rooms_view),
+#     # path("list/", views.ListRoomsView.as_view()),
+#     # path("<int:pk>/", views.DetailRoomView.as_view()),
+#     path("search/", views.room_search, name="search"),
+#     path("", views.RoomsView.as_view()),
+#     path("<int:pk>/", views.RoomView.as_view()),
+# ]
--- rooms/views.py
+++ rooms/views.py
@@ -5,9 +5,26 @@ from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
 from .models import Room
 from .serializers import RoomSerializer, WriteRoomSerializer
+from .permissions import IsOwner
+
+class RoomViewSet(ModelViewSet):
+
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
 
 
 class OwnPagination(PageNumberPagination):
